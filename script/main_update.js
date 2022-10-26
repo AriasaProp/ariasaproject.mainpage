a197590edc5da698cea86363a23a26f45a865ee8
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2022-10-26 15:35:00 +0800

    add content path

--- script/main_update.js
+++ script/main_update.js
@@ -1,5 +1,9 @@
 var datetimeSpan = document.getElementById('datetime');
-const dateFormat = {hour:'2-digit', minute:'2-digit', weekday:'long', day:'2-digit', month:'short', year:'numeric'};
+
+var hourTick = document.getElementById('hour-tick');
+var minuteTick = document.getElementById('minute-tick');
+
+const dateFormat = {weekday:'long', day:'2-digit', month:'short', year:'numeric'};
 
 update();
 let nIntervId = setInterval(update(), 60000); // per minutes = 60000 ms
@@ -8,4 +12,8 @@ let nIntervId = setInterval(update(), 60000); // per minutes = 60000 ms
 function update() {
 	let d = new Date();
 	datetimeSpan.innerHTML = d.toLocaleString('id', dateFormat);
+	let hourDeg = 360.0 * d.getHours() / 12;
+	let minuteDeg = 360.0 * d.getMinutes() / 60;
+	hourTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+hourDeg+')');
+	minuteTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+minuteDeg+')');
 }
