750ba38331e1f2890e8755c20b9aceafc802c45c
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-10-27 14:06:09 +0800

    new

--- script/main_update.js
+++ script/main_update.js
@@ -3,17 +3,25 @@ var datetimeSpan = document.getElementById('datetime');
 var hourTick = document.getElementById('hour-tick');
 var minuteTick = document.getElementById('minute-tick');
 
-const dateFormat = {weekday:'long', day:'2-digit', month:'short', year:'numeric'};
+const dateFormat = {
+	weekday: 'long',
+	day: '2-digit',
+	month: 'short',
+	year: 'numeric'
+};
 
 update();
-let nIntervId = setInterval(update(), 60000); // per minutes = 60000 ms
-
+let nIntervId = setInterval(update(), 1000); // per minutes = 60000 ms
+window.addEventListener('beforeunload', function(){
+	clearInterval(nIntervId);
+});
 
 function update() {
 	let d = new Date();
 	datetimeSpan.innerHTML = d.toLocaleString('id', dateFormat);
-	let hourDeg = 360.0 * d.getHours() / 12;
-	let minuteDeg = 360.0 * d.getMinutes() / 60;
-	hourTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+hourDeg+')');
-	minuteTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+minuteDeg+')');
+	let secp = 6.0 * d.getSeconds();
+	let mntp = 6.0 * d.getMinutes() + secp / 60.0;
+	let hourp = 30.0 * d.getHours() + mntp / 12.0;
+	minuteTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+mntp+')');
+	hourTick.setAttribute('transform', 'matrix(1 0 0 1 100 100) rotate('+hourp+')');
 }
